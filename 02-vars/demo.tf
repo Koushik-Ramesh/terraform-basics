100b04587bd7d0b7c18fcfceb20381510f37e253
Koushik-Ramesh <koushik.r9380@gmail.com>
2024-01-08 01:09:46 +0530

    Autopush

--- 02-vars/demo.tf
+++ 02-vars/demo.tf
@@ -8,3 +8,18 @@ variable "Sample" {
 output "op" {
     value = var.Sample
 }
+
+# Whenever there is a need to print variable in group of words, you need to use the bash syntax
+output "ops" {
+    value = "My first message is ${var.Sample}"
+}
+
+# In terraform, there is no concept of single quotes
+
+variable "number" {
+    value = 100
+}
+
+output "op_number" {
+    value = var.number
+}

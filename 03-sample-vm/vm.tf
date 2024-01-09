9773c20d30d879627001e99404963803841b50b8
Koushik-Ramesh <koushik.r9380@gmail.com>
2024-01-10 00:46:35 +0530

    Autopush

--- 03-sample-vm/vm.tf
+++ 03-sample-vm/vm.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
   ami           = "ami-0f3cbcc10e8b99542"
-  instance_type = "t3.micro"
+  instance_type = "t3.medium"
 
   tags = {
     Name = "Terraform-Instance"

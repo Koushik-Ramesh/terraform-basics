a8d8818f5f87e7a9b66b26c31bf212a7f2b48f98
Koushik-Ramesh <koushik.r9380@gmail.com>
2024-01-27 19:39:24 +0530

    Autopush

---
+++ 04-remote-state/main.tf
@@ -0,0 +1,17 @@
+# DataDource to fetch the info of AMI
+data "aws_ami" "example" {
+    executable_users = ["self"]
+    most_recent      = true
+    name_regex       = "b55 - ansible-lab-image"
+    owners           = ["self"]
+}
+
+resource "aws_instance" "web" {
+  count         = var.howManyYouWant
+  ami           = ams_ami.ami.image_id
+  instance_type = "t3.medium"
+
+  tags = {
+    Name = "Terraform-web-Instance"
+  }
+}

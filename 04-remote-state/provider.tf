05ec5490bbfd2dcfeaa437d9cc7415307f0a8b31
Koushik-Ramesh <koushik.r9380@gmail.com>
2024-01-27 20:02:43 +0530

    Autopush

--- 04-remote-state/provider.tf
+++ 04-remote-state/provider.tf
@@ -1,10 +1,11 @@
 provider "aws" {}
 
-terraform {
-    backend "s3" {
-        bucket = "b55-terraform-state"
-        key = "dev/ec2/terraform.tfstate"
-        region = "us-east-1"
-    }
-}
+
+//terraform {
+//    backend "s3" {
+//       bucket = "b55-terraform-state"
+//        key = "dev/ec2/terraform.tfstate"
+//        region = "us-east-1"
+//    }
+//}
 

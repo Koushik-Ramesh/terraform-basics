5de75813122a7790ea7a3e4acac67806ad3cfa1b
Koushik-Ramesh <koushik.r9380@gmail.com>
2024-01-27 19:45:11 +0530

    Autopush

--- 04-remote-state/main.tf
+++ 04-remote-state/main.tf
@@ -8,7 +8,7 @@ data "aws_ami" "example" {
 
 resource "aws_instance" "web" {
   count         = var.howManyYouWant
-  ami           = data.ams_ami.ami.image_id
+  ami           = data.aws_ami.ami.image_id
   instance_type = "t3.medium"
 
   tags = {

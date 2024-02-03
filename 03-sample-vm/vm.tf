2433f2837152e1f48dd18e74dce65b70acb5727d
Koushik-Ramesh <koushik.r9380@gmail.com>
2024-02-03 19:19:26 +0530

    Autopush

--- 03-sample-vm/vm.tf
+++ 03-sample-vm/vm.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "app" {
   ami           = "ami-0f3cbcc10e8b99542"
   instance_type = "t3.medium"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
   tags = {
     Name = "Terraform-Instance"

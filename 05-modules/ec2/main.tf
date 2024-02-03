8ff086b053b0afbe281a22a0cdf676b121937fb1
Koushik-Ramesh <koushik.r9380@gmail.com>
2024-02-03 20:16:26 +0530

    Autopush

--- 05-modules/ec2/main.tf
+++ 05-modules/ec2/main.tf
@@ -1,5 +1,5 @@
-# Creates EC@
-resource "aws_instance" "app" {
+# Creates EC2
+resource "aws_instance" "moapp" {
   ami           = "ami-0f3cbcc10e8b99542"
   instance_type = "t3.medium"
   vpc_security_group_ids = [var.sg]
